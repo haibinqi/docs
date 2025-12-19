2360a67ddcdca6ea6ec89985cde59899dff6f376
haibinqi <qihaibintc@gmail.com>
2025-12-19 15:38:56 +0800

    svg

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,10 +1,14 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "../build/server";
 
-// Force recompile 28
+// Force recompile 30
 export const onRequest = createPagesFunctionHandler({
   build,
-  getLoadContext({ env }) {
-    return env as unknown as any;
+  getLoadContext: (context) => {
+    return {
+      cloudflare: {
+        env: context.env
+      }
+    };
   },
 });
